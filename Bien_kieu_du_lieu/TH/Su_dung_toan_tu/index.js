0ec628a42e757f1014c3bc54b4b36100f9e01684
cuong <hahuycuong1002@gmail.com>
2021-06-21 16:34:22 +0700

    add

---
+++ Bien_kieu_du_lieu/TH/Su_dung_toan_tu/index.js
@@ -0,0 +1,9 @@
+inputWidth = prompt("Nhập width");
+inputHeight = prompt("Nhập height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích hình chữ nhật là : " + area + "<br/>");
+
+document.write("Chiều dài hình chữ nhật là : " + width + "<br/>");
+document.write("Chiều cao hình chữ nhật là : " + height);
